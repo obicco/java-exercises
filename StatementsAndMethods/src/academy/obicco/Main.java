343c132ed014592f662e27c3c681df43d2585ef7
obicco <24893231+obicco@users.noreply.github.com>
2022-06-12 14:01:48 +0200

    Methods

--- StatementsAndMethods/src/academy/obicco/Main.java
+++ StatementsAndMethods/src/academy/obicco/Main.java
@@ -7,8 +7,9 @@ public class Main {
         int levelCompleted = 5;
         int bonus = 100;
 
-        calculateScore(gameOver, score, levelCompleted, bonus);
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
         calculateScore(true, 10000, 8, 200);
+        System.out.println("Your final score was " + highScore);
 
         if (score < 5000 && score > 1000) {
             System.out.println("Your score was less than 5000 but greater than 1000");
@@ -21,9 +22,7 @@ public class Main {
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         if (gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-            return finalScore;
+            return score + (levelCompleted * bonus);
         } else return -1;
     }
 }
